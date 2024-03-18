174d388e65620c1954c34d4527cfe235cfdb5e52
RyzeNGrind <14363544+RyzeNGrind@users.noreply.github.com>
2024-03-18 19:42:23 -0400

    Fixed cached failure of attrib.

--- flake.nix
+++ flake.nix
@@ -25,15 +25,28 @@
         system = "x86_64-linux";
         overlays = [ ];
       };
+      
+      # Assuming vm-test.nix returns a NixOS configuration
+      vmConfig = std.growOn {
+        inherit inputs;
+        cellsFrom = ./nix;
+        cellBlocks = with std.blockTypes; [
+          (nixosConfiguration "vm-test" ./nix/hosts/vm-test.nix)
+          # Add other configurations as needed
+        ];
+      }.nixosConfigurations.vm-test;
+      
+      # Create the VM startup script
+      runVmScript = pkgs.writeShellScript "run-vm.sh" ''
+        export NIX_DISK_IMAGE=$(mktemp -u -t nixos.qcow2)
+        trap "rm -f $NIX_DISK_IMAGE" EXIT
+        ${vmConfig.config.system.build.vm}/bin/run-nixos-vm
+      '';
     in
     {
-      apps = {
-        x86_64-linux = {
-          vm = {
-            type = "app";
-            program = "${self.nixosConfigurations.vm-test.config.system.build.vm}/bin/run-nixos-vm";
-          };
-        };
+      apps.x86_64-linux.vm = {
+        type = "app";
+        program = runVmScript;
       };
 
       defaultApp.x86_64-linux = self.apps.x86_64-linux.vm;

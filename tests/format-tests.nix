033c777364c61f05bdeb2c2b87db93d022133e6a
RyzeNGrind <14363544+RyzeNGrind@users.noreply.github.com>
2025-01-08 16:36:53 -0500

    testing

--- tests/format-tests.nix
+++ tests/format-tests.nix
@@ -5,7 +5,7 @@ let
   inherit (pkgs) lib;
   
   # Helper function to test docker images
-  testDocker = image: pkgs.vmTools.runInLinux {
+  testDocker = image: pkgs.vmTools.runInLinuxVM {
     inherit (pkgs) system;
     memSize = 1024;
     
@@ -18,7 +18,7 @@ let
       echo "docker" >> $out/nix-support/hydra-build-products
     '';
     
-    buildInputs = [ pkgs.docker ];
+    buildInputs = with pkgs; [ docker ];
     
     script = ''
       # Start docker daemon
@@ -35,7 +35,7 @@ let
   };
   
   # Helper function to test ISO images
-  testISO = iso: pkgs.vmTools.runInLinux {
+  testISO = iso: pkgs.vmTools.runInLinuxVM {
     inherit (pkgs) system;
     memSize = 2048;
     
@@ -48,7 +48,7 @@ let
       echo "iso" >> $out/nix-support/hydra-build-products
     '';
     
-    buildInputs = [ pkgs.qemu ];
+    buildInputs = with pkgs; [ qemu ];
     
     script = ''
       # Test ISO boot in QEMU
@@ -62,7 +62,9 @@ let
   };
   
   # Helper function to test kexec bundles
-  testKexec = bundle: pkgs.runCommand "test-kexec" {} ''
+  testKexec = bundle: pkgs.runCommand "test-kexec" {
+    buildInputs = [ pkgs.kexec-tools ];
+  } ''
     # Verify bundle structure
     if [ ! -f ${bundle}/kernel ]; then
       echo "Kernel not found in kexec bundle"
@@ -73,15 +75,40 @@ let
       exit 1
     fi
     
+    # Test kexec load (dry run)
+    kexec -l ${bundle}/kernel --initrd=${bundle}/initrd --reuse-cmdline -t
+    
     mkdir -p $out/nix-support
     touch $out/nix-support/hydra-build-products
     echo "kexec" >> $out/nix-support/hydra-build-products
   '';
   
   # Helper function to test SD card images
-  testSDImage = image: pkgs.runCommand "test-sd-image" {} ''
+  testSDImage = image: pkgs.runCommand "test-sd-image" {
+    buildInputs = with pkgs; [ qemu ];
+    nativeBuildInputs = with pkgs; [ util-linux ];
+  } ''
     # Verify image format and structure
-    ${pkgs.qemu}/bin/qemu-img info ${image}
+    qemu-img info ${image}
+    
+    # Try to mount the image (read-only)
+    mkdir -p tmpmnt
+    if [ -f ${image} ]; then
+      # Get the start sector of the first partition
+      SECTOR=$(fdisk -l ${image} | grep Linux | head -n1 | awk '{print $2}')
+      OFFSET=$((SECTOR * 512))
+      
+      # Mount the first partition read-only
+      mount -o ro,offset=$OFFSET ${image} tmpmnt
+      
+      # Check for basic files
+      if [ ! -d tmpmnt/boot ]; then
+        echo "No boot directory found"
+        exit 1
+      fi
+      
+      umount tmpmnt
+    fi
     
     mkdir -p $out/nix-support
     touch $out/nix-support/hydra-build-products
